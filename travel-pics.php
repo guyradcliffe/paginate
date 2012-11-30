406f86ad7a67f13624f99d1ed6ecaae40ffa3e64
Guy Radcliffe <gr7432@gmail.com>
2012-11-30 15:30:30 -0500

    changes to insert & pics

--- travel-pics.php
+++ travel-pics.php
@@ -4,9 +4,18 @@ include("common/header.php");
 ?>
 <div id="leftdiv">
 <div class="mapholder">
-	<h5 style="color:#2a2a2a; margin-bottom:7px;">Where is Port-au-Prince?</h5>
-	<iframe width="230" height="175" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;t=m&amp;ll=18.427502,-72.103271&amp;spn=1.823901,2.515869&amp;z=7&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;t=m&amp;ll=18.427502,-72.103271&amp;spn=1.823901,2.515869&amp;z=7&amp;source=embed" style="color:#0000FF;text-align:left"target="_blank">View Larger Map</a></small>
-</div>
+<?php
+    if ($_GET['location']==$location) {
+      $maptext = mysql_query ("SELECT SQL_CALC_FOUND_ROWS * FROM textTbl WHERE location='$location'"); // Run the query.
+      if ($maptext) {
+        	while ($row = mysql_fetch_array($maptext)) {
+    			echo "<h5 style='color:#2a2a2a; margin-bottom:7px;'>" . $row[maptext] . "</h1>" . PHP_EOL;
+          echo "<iframe width='230' height='175' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='" . $row[map] . "'></iframe><br /><small><a href='" . $row[map] . "' style='color:#0000FF;text-align:left'target='_blank'>View Larger Map</a></small>" . PHP_EOL;				
+    		}
+    	}
+  	}
+  ?>
+</div><!-- end mapholder -->
 <div class="adspacer"></div>
 <script type="text/javascript"><!--
 google_ad_client = "ca-pub-4537884625055504";
@@ -36,24 +45,25 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 
 </div><!-- end leftdiv -->
 <div id="rightdiv">
-	<h1 style="font-size:24px; margin-bottom:5px;">Pre Earthquake Haiti Photos</h1>
-	<p>
-		I made several trips from Philadelphia to Port-au-Prince, Haiti between 2002 and 2004.
-	</p>
 
 <?php
 $page = $_GET['page'];
 $row_count = 0;
 function getPics ($location, $table, $page) {
   if ($_GET['location']==$location) {
-    //$result = mysql_query ("SELECT SQL_CALC_FOUND_ROWS * FROM $table ORDER BY link ASC LIMIT $page, 12"); // Run the query.
-    $result = mysql_query ("SELECT SQL_CALC_FOUND_ROWS * FROM haitiTbl ORDER BY link ASC LIMIT $page, 12"); // Run the query.
+    $gettext = mysql_query ("SELECT SQL_CALC_FOUND_ROWS * FROM textTbl WHERE location='$location'"); // Run the query.
+    if ($gettext) {
+      	while ($row = mysql_fetch_array($gettext)) {
+  			echo "<h1>" . $row[h1] . "</h1><div>" . $row[text] . "</div>" . PHP_EOL;				
+  		}
+  	}
+    $result = mysql_query ("SELECT SQL_CALC_FOUND_ROWS * FROM $table ORDER BY link ASC LIMIT $page, 12"); // Run the query.
     $countData = mysql_fetch_assoc(mysql_query("SELECT found_rows() AS total"));
   	$totalRows = $countData['total'];
   	if ($result) {
   		echo "<div class='textphoto'>" . PHP_EOL;
       	while ($row = mysql_fetch_array($result)) {
-  			echo "<a href='" . $row[link] . "' rel='prettyPhoto[tanzania]' title='" . $row[caption] . "'><img src='" . $row[link] . "' alt='" . $row[alt] . "' class='" . $row[css_class] . "' /></a>" . PHP_EOL;				
+  			echo "<a href='" . $row[link] . ".jpg' rel='prettyPhoto[]' title='" . $row[caption] . "'><img src='" . $row[link] . "-thumb.jpg' alt='" . $row[alt] . "' /></a>" . PHP_EOL;				
   			$row_count++;
         if (($row_count %4==0)&&($row_count %12!=0)) {
   			 echo "</div><div class='textphoto'>";
@@ -67,7 +77,7 @@ function getPics ($location, $table, $page) {
 }
 
 getPics('boca-chica','bocachica2008',$page);
-getPics('nairobi-arboretum', 'nairobi-table', 'nairobi, kenya', $page);
+getPics('nairobi-arboretum','kenya',$page);
 getPics('pre-earthquake-haiti','haitiTbl',$page);
 
 echo "</div>";// end div right
